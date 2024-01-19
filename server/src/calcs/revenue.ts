1460c10478075e5a0e5e3e2bd2a3eb7339c04ab3
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-19 15:03:08 -0300

    refactor mrr calculation to consider late, cancelled and yearly customers

--- server/src/calcs/revenue.ts
+++ server/src/calcs/revenue.ts
@@ -29,20 +29,42 @@ export const getMonthlyRecursiveRevenue = (
     year,
   );
 
-  const activeSubscriptions = monthSubscriptions.filter(
-    (s) => s.status === 'Active' || s.status === 'Upgrade',
-  );
-
   let monthlyRevenue = 0;
-  activeSubscriptions.map((s) => {
-    if (s.chargeFrequencyInDaysField === 30) {
+  monthSubscriptions.map((s) => {
+    if (
+      s.chargeFrequencyInDaysField === 30 &&
+      (s.status === 'Active' || s.status === 'Upgrade') // get active and upgrade customers
+    ) {
       return (monthlyRevenue += s.valueCharged);
     }
+
     if (
-      s.chargeFrequencyInDaysField === 360 ||
-      s.chargeFrequencyInDaysField === 365
+      s.chargeFrequencyInDaysField === 30 &&
+      s.status === 'Late' && // sum late customers payments and skips last one
+      s.statusDate.getMonth() > monthIndex &&
+      s.statusDate.getFullYear() >= year
     ) {
-      return (monthlyRevenue += s.valueCharged / 12);
+      return (monthlyRevenue += s.valueCharged);
+    }
+
+    if (
+      s.chargeFrequencyInDaysField === 30 &&
+      s.status === 'Canceled' && // sum canceled customers payments and skips last one
+      s.startDate.getMonth() <= monthIndex &&
+      s.startDate.getFullYear() <= year &&
+      s.cancellationDate.getMonth() > monthIndex &&
+      s.cancellationDate.getFullYear() >= year
+    ) {
+      return (monthlyRevenue += s.valueCharged);
+    }
+
+    if (
+      (s.chargeFrequencyInDaysField === 360 ||
+        s.chargeFrequencyInDaysField === 365) && // sum yearly customers payments
+      s.startDate.getMonth() === monthIndex &&
+      s.startDate.getFullYear() === year
+    ) {
+      return (monthlyRevenue += s.valueCharged);
     }
   });
 
